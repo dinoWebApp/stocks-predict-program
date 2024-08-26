62afba4ff0dc00bbb85eaeee9bb0157868f1d867
nomadWebDeveloper <kgjsmart1559@gmail.com>
2024-08-27 08:01:44 +0900

    index page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as exphbs from 'express-handlebars';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.engine(
+    'hbs',
+    exphbs.create({
+      extname: 'hbs',
+      defaultLayout: null, // 기본 레이아웃 사용 안 함
+      partialsDir: join(__dirname, '..', 'views/partials'), // Partial 디렉토리 설정
+    }).engine,
+  );
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(3000);
 }
 bootstrap();
